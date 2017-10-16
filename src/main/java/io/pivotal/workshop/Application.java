6f37219123ce6006ee21d43420c3f11c7ef7a61d
ganesanbbc <sugan1983>
2017-10-16 17:20:33 +0530

    disable oAuth

--- src/main/java/io/pivotal/workshop/Application.java
+++ src/main/java/io/pivotal/workshop/Application.java
@@ -2,9 +2,8 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableResourceServer
+//@EnableResourceServer
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
